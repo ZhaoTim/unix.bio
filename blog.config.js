093a462b6f647253d8cbbdbc9c10777f615bd192
zhaomeicheng <zhaomeicheng@xueqiu.com>
2022-04-10 22:07:55 +0800

    feat: update blog config

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  author: 'Witt',
-  title: 'Witt - unix.bio',
+  author: 'Tim Zhao',
+  title: 'Tim Zhao',
   description:
     "Hi, I'm Witt. A JavaScript full stack engineer, I'm creating apps and libs to help improve the developer experience.",
-  summary: 'JavaScript 工程师，对开发者体验感兴趣。',
+  summary: 'It baffles me, this world. I dont want to leave it yet',
   language: 'zh-cn',
   latestLimit: 5,
 
@@ -13,14 +13,14 @@ const BLOG = {
     list: '所有',
   },
 
-  email: 'unix.bio@gmail.com',
-  github: 'unix',
-  twitter: 'echo_witt',
-  domain: 'unix.bio',
+  email: 'zhaomeichengq@gmail.com',
+  // github: '',
+  // twitter: '',
+  domain: 'zhaomeicheng.com',
 
   googleAnalytics: 'UA-110371817-3',
   cn: true,
-  enableViews: true,
+  enableViews: false,
 
   layouts: {
     pageWidth: '750px',
