b7ca881c4aeb8da7ddab97b3aa02ef07b015031d
zhaomeicheng <zhaomeicheng@xueqiu.com>
2022-04-10 23:37:43 +0800

    update

---
+++ pages/posts/components/Gallery/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  Drawer,
+  Button,
+  Divider,
+  Spacer,
+  Toggle,
+  Text,
+  Image,
+  Display,
+} from '@geist-ui/core'
+import { Image as ImageIcon } from '@geist-ui/icons'
+import Grid from '@geist-ui/core/esm/grid/grid';
+const Gallery = () => {
+  const [visible, setVisible] = React.useState(false);
+
+  const toggle = () => setVisible(_ => !_);
+  return (
+    <>
+      <Drawer
+        // style={{ width: "300px" }}
+        onClose={() => setVisible(false)}
+        visible={visible} placement="right">
+        <Drawer.Title>🌸 关于春天的影像 🌸</Drawer.Title>
+        <Drawer.Content>
+          <Display shadow caption="老大爷 - 永恒的主题">
+            <Image width="300px" src="https://static.zhaomeicheng.com/yeye.jpeg" />
+          </Display>
+          <Display shadow caption="公司楼下">
+            <Image width="300px" src="https://static.zhaomeicheng.com/under_company.jpeg" />
+          </Display>
+        </Drawer.Content>
+      </Drawer>
+      <Spacer h={2} />
+      <Grid.Container justify="center">
+        <Button icon={<ImageIcon />} onClick={toggle} ghost auto>🌸 关于春天的影像 🌸</Button>
+      </Grid.Container>
+    </>
+  )
+}
+
+export default Gallery;
